6c563c6b4fa9e03d02bec041e445a298ef5a6e40
Thong <20127634@student.hcmus.edu.vn>
2022-06-22 20:07:55 +0700

    Lan 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostItem from './components/PostItem';
+// import Hihi from './components/Usestate';
+// import ToDoList from './components/Todolist';
+function App() {
+  return (
+    <div className="App">
+     <PostItem />
+    </div>
+  );
+}
+export default App;
