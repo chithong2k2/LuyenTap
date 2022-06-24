2b4a4e790f31f1511288616b5b406f6f1dd1169e
Thong <20127634@student.hcmus.edu.vn>
2022-06-24 21:10:40 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import PostItem from './components/PostItem';
-// import Hihi from './components/Usestate';
-// import ToDoList from './components/Todolist';
+import Usestate from './components/Usestate';
+import ToDoList from './components/Todolist';
+import User from './components/user/User';
+import Navbar from './components/navbar/Navbar';
+import Header from './components/header/Header';
 function App() {
   return (
     <div className="App">
-     <PostItem />
+      <Navbar/>
+      <Header/>
     </div>
   );
 }
