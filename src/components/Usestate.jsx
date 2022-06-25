f59d5caac2def7f70bfef5679748197bd4902067
Thong <20127634@student.hcmus.edu.vn>
2022-06-25 11:55:56 +0700

    update

--- src/components/Usestate.jsx
+++ src/components/Usestate.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {useState} from 'react';
+import './usestate.css'
 function Hihi()
 { 
     const [count, setCount] = useState(0);
@@ -10,10 +11,14 @@ function Hihi()
         setCount(prev => prev-1)
     }
     return(
-        <div>
-            <h1>{count}</h1>
-            <button onClick={inCrease}>Tăng</button>
-            <button onClick={decrease}>Giảm</button>
+        <div className="Time">
+            <div className="Content">
+                <h1>{count}</h1>
+            </div>
+            <div className="Click">
+                <button className="Increase" onClick={inCrease}>Tăng</button>
+                <button className="Decrease" onClick={decrease}>Giảm</button>
+            </div>
         </div>
     )
 }
